14deabe626f1a614a52945bf596f972b66eae568
vikas-saini-7 <vikas289888@gmail.com>
2025-08-09 17:01:07 +0530

    sm

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 // next.config.js
-import createMDX from '@next/mdx'
+import createMDX from "@next/mdx";
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
@@ -7,12 +7,12 @@ const withMDX = createMDX({
     remarkPlugins: [],
     rehypePlugins: [],
   },
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   // other Next.js config options here
-}
+};
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
